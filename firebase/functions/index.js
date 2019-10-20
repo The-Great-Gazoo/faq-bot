e37653b30b3a19ab6cc5bc63c08a296dc55a3130
Yibo Yu <yiboyu1@gmail.com>
2019-10-20 05:30:07 -0400

    Added scheduled function to refresh access token every hour

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,3 +1,4 @@
+
 const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
@@ -7,8 +8,27 @@ const functions = require('firebase-functions');
 //  response.send("Hello from Firebase!");
 // });
 
-/*
 exports.refreshGenesysAPIToken = functions.pubsub.schedule('every 59 minutes').onRun((context) => {
-    console.log('This will be run every 5 minutes!');
+    var request = require("request");
+
+    var options = { 
+        method: 'POST',
+        url: 'https://api.genesysappliedresearch.com/v2/knowledge/generatetoken',
+        headers: {   
+            'cache-control': 'no-cache',
+            secretkey: '8567a7ee-8623-4b92-a112-17f0a755572a',
+            organizationid: '507c6b94-d35a-48ce-9937-c2e4aa69c279' 
+        } 
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        const ref = firebase.firestore().collection('config').doc('genesys-api');
+        ref.get().then((doc) => {
+            if(doc.exists()){
+                ref.update({token: body.token});
+            }
+        });
+    });
 });
-*/
